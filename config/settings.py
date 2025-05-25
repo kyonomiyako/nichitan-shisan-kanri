ac884b4556ba9bc3769f3342a67ade981490be0c
kyonomiyako <kinginkaku@gmail.com>
2025-05-25 23:23:12 +0900

    Add Render URL to ALLOWED_HOSTS

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,8 @@ SECRET_KEY = "django-insecure-b!fqu-1p0&y^l$m_%+7@)kh_2eqt-e&y-8^)x@tuu4so07!g1f
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['nichitan-app.onrender.com', 'localhost', '127.0.0.1']
+
 
 
 # Application definition
